30220550f6227361e2ded93fc4a53f93448207e8
Florence Nway <38194221+FlorenceNway@users.noreply.github.com>
2021-01-21 23:41:15 +0000

    delete user test

--- src/store/reducers/register.reducer.test.js
+++ src/store/reducers/register.reducer.test.js
@@ -1,15 +1,19 @@
 import registerReducer from './register.reducer';
 
+const initialState = {
+    users: []
+}
+
 describe('Register Reducer', ()=>{
     it('should return default state', () => {
-        const newState = registerReducer(undefined, {});
+        const newState = registerReducer(initialState, {});
         expect(newState.users).toEqual([]);
     })
 
     it('should return new state if receiving ADD_USER type', () => {
         const username = 'florence';
 
-        const newState = registerReducer(undefined, {
+        const newState = registerReducer(initialState, {
             type: 'ADD_USER',
             payload: username
         })
@@ -17,14 +21,14 @@ describe('Register Reducer', ()=>{
     })
 
     it('should return new state if receiving DELETE_USER type', () => {
-        let usersState = {users:['florence', 'nway']}
+        let usersState =  {users:[{id:1,name:'florence'},{id:2, name:'nway'}]}
         const id = 2;
 
         const newState = registerReducer({...usersState}, {
             type: 'DELETE_USER',
             payload: id
         })
-        console.log(newState.users[0], id)
-        expect(newState.users).toEqual(['florence'])
+        
+        expect(newState.users[0].name).toEqual('florence')
     })
 })
