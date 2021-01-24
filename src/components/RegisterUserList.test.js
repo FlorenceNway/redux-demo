9e056a1a8d49738cbc32607d786cdd637d9b3f20
Florence Nway <FlorenceNway@users.noreply.github.com>
2021-01-24 23:46:38 +0000

    component testing

--- src/components/RegisterUserList.test.js
+++ src/components/RegisterUserList.test.js
@@ -1,37 +1,41 @@
 import React from "react";
-import { shallow, mount } from "enzyme";
-import RegisterUserList from "./RegisterUserList";
 import { Provider } from "react-redux";
-import {createStore} from "redux";
+import { mount } from "enzyme";
+import RegisterUserList from "./RegisterUserList";
+import { createStore } from "redux";
 import users from "../store/reducers/register.reducer";
-import App from "../App";
 
 const findByTestAtrr = (component, attar) => {
-    const wrapper = component.find(`[data-test='${attar}']`);
-    return wrapper;
-  };
+  const wrapper = component.find(`[data-test='${attar}']`);
+  return wrapper;
+};
 
 describe("RegisterUserList Component", () => {
   describe("Component Renders", () => {
     let wrapper;
+    let initialState = { users: [{ id: 1, name: "Alex" }] };
     beforeEach(() => {
-        const store = createStore(users)
-      wrapper = mount(<Provider store={store}><RegisterUserList/></Provider>);
+      const store = createStore(users, initialState);
+
+      wrapper = mount(
+        <Provider store={store}>
+          <RegisterUserList />
+        </Provider>
+      );
+      // console.log(wrapper.debug());
     });
 
     it("should render without error", () => {
       const component = findByTestAtrr(wrapper, "list-wrapper");
-      expect(component.length).toBe(1);
+      expect(component.length).toEqual(1);
     });
     it("should render username", () => {
       const username = findByTestAtrr(wrapper, "username");
-      console.log(username)
-      expect(username.length).toBe(1);
+      expect(username.length).toEqual(1);
     });
     it("should render buttons div", () => {
       const buttons = findByTestAtrr(wrapper, "buttons");
       expect(buttons.length).toBe(1);
     });
   });
-
 });
